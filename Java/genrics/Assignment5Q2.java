58ffdff45191ff4c54c1663f9d446ce4898c2568
barath <barathmuthukumar2k@gmail.com>
2022-02-21 08:33:02 +0530

    added exception assignments and genrics assignment

---
+++ Java/genrics/Assignment5Q2.java
@@ -0,0 +1,24 @@
+package com.practicejava;
+import java.util.HashMap;
+public class Assignment5Q2 {
+    public static void main(String[] args) {
+        HashMap<Integer,Double> hm = new HashMap();
+        hm.put(1,1.0);
+        hm.put(2,2.0);
+        hm.put(3,3.0);
+        hm.put(4,4.0);
+        hm.put(5,5.0);
+        hm.put(6,6.0);
+        hm.put(7,7.0);
+        hm.put(8,8.0);
+        hm.put(9,9.0);
+        hm.put(10,10.0);
+
+        System.out.println(hm);
+
+
+
+
+    }
+
+}
