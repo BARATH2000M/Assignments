9a55f312981ef8a55fc6b62fea3fea5b34962243
barath <barathmuthukumar2k@gmail.com>
2022-04-08 21:08:14 +0530

    added javascript and es6

---
+++ java script/logical/Q2/script.js
@@ -0,0 +1,51 @@
+// alert("welcome to Js");
+
+// console.log("welcome to india")
+
+// program to check if the number is even or odd
+for (let i = 0; i < 3; i++){
+const number = prompt("Enter a number: ");
+
+
+if((number <30)&& (number>1)){
+    document.write("*".repeat(number));
+    document.write ("<br>");
+}
+
+}
+
+// let a=Math.max(1,56,2,3);
+// console.log(a);
+
+// function count_ht(s, N)
+// {
+     
+//     // Check if initially all the
+//     // coins are facing towards head
+//     var p = [0,0];
+//     if(s == 'H')
+//     {
+//         p[0] = Math.floor(N / 2.0);
+//         p[1] = Math.ceil(N / 2.0);
+//     }
+     
+//     // Check if initially all the coins
+//     // are facing towards tail
+//     else if(s == 'T')
+//     {
+//         p[0] = Math.ceil(N / 2.0);
+//         p[1] = Math.floor(N / 2.0);
+//     }
+     
+//     return p;
+// }
+ 
+// // Driver code
+// var C = 'H';
+// var N = 7;
+ 
+// var p = count_ht(C, N);
+ 
+// document.write( "Head = " + (p[0]) + "<br>");
+// document.write( "Tail = " + (p[1]) + "<br>");
+
